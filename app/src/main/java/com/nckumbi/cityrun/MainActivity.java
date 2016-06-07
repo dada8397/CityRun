5209257fab168e9da0637768043a0f245331ebcf
0140454 <s1994928@yahoo.com.tw>
2016-06-07 12:55:23 +0800

    Prevent display of navbar when touching the screen
    Reference: http://stackoverflow.com/a/29699637

--- app/src/main/java/com/nckumbi/cityrun/MainActivity.java
+++ app/src/main/java/com/nckumbi/cityrun/MainActivity.java
@@ -38,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
             final View decorView = getWindow().getDecorView();
             final int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
             decorView.setSystemUiVisibility(uiOptions);
             decorView.setOnSystemUiVisibilityChangeListener
                     (new View.OnSystemUiVisibilityChangeListener() {
