f96cab4fb26d1e7cf6fceace2014ed1633ccdb55
0140454 <s1994928@yahoo.com.tw>
2016-06-08 15:27:39 +0800

    Finish register function

--- app/src/main/java/com/nckumbi/cityrun/RegisterActivity.java
+++ app/src/main/java/com/nckumbi/cityrun/RegisterActivity.java
@@ -1,19 +1,23 @@
 package com.nckumbi.cityrun;
 
 import android.app.ActivityManager;
+import android.app.ProgressDialog;
 import android.content.ComponentName;
 import android.content.Context;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nckumbi.cityrun.utils.ProfileHelper;
+import com.nckumbi.cityrun.utils.Utils;
+
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -59,6 +63,7 @@ public class RegisterActivity extends AppCompatActivity {
         policyCheckBox = (CheckBox) findViewById(R.id.policyCheckBox);
 
         regBackImageButton.setOnClickListener(backImageButtonOnClick);
+        regRegisterImageButton.setOnClickListener(registerImageButtonOnClick);
     }
 
     @Override
@@ -79,7 +84,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if(MainActivity.stopped) {
+        if (MainActivity.stopped) {
             MainActivity.player = new BackgroundMusicPlayer(RegisterActivity.this, R.raw.main_bgm, true);
             MainActivity.player.execute();
             MainActivity.stopped = false;
@@ -95,6 +100,54 @@ public class RegisterActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             RegisterActivity.this.finish();
+        }
+    };
+
+    protected View.OnClickListener registerImageButtonOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (regUserNameEditText.getText().toString().isEmpty()
+                    || regEmailEditText.getText().toString().isEmpty()
+                    || regPasswordEditText.getText().toString().isEmpty()
+                    || regConfirmPasswordEditText.getText().toString().isEmpty()) {
+                Utils.showErrorDialog(RegisterActivity.this, "請輸入完整的註冊資訊！");
+            } else if (!regPasswordEditText.getText().toString().equals(
+                    regConfirmPasswordEditText.getText().toString())) {
+                Utils.showErrorDialog(RegisterActivity.this, "兩次密碼不一致！");
+            } else if (!policyCheckBox.isChecked()) {
+                Utils.showErrorDialog(RegisterActivity.this, "您必須接受隱私權政策才能繼續使用！");
+            } else {
+                final ProgressDialog progressDialog = ProgressDialog.show(RegisterActivity.this,
+                        "請稍後", "等待伺服器回應中...", true);
+
+                ProfileHelper.register(regUserNameEditText.getText().toString(),
+                        regEmailEditText.getText().toString(),
+                        regPasswordEditText.getText().toString(),
+                        new ProfileHelper.Callback() {
+                            @Override
+                            public void onComplete(JSONObject result) {
+                                progressDialog.dismiss();
+
+                                if (result == null) {
+                                    Toast.makeText(RegisterActivity.this, R.string.msg_network_error,
+                                            Toast.LENGTH_LONG).show();
+                                } else {
+                                    try {
+                                        if (result.getBoolean("result")) {
+                                            Toast.makeText(RegisterActivity.this,
+                                                    "註冊成功，即將返回註冊頁面。", Toast.LENGTH_LONG).show();
+
+                                            RegisterActivity.this.finish();
+                                        } else {
+                                            Utils.showErrorDialog(RegisterActivity.this,
+                                                    R.string.msg_register_fail);
+                                        }
+                                    } catch (Exception e) {
+                                    }
+                                }
+                            }
+                        });
+            }
         }
     };
 }
